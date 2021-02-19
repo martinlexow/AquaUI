c406be5bf8a03912a7a5a7e2a7519a906f05e15c
Helge Heß <helge@alwaysrightinstitute.com>
2021-02-19 01:40:32 +0100

    Update AquaBubbleBackground.swift
    Decoupling the shape, but the inset doesn't look quite right yet.

--- AquaBubbleBackground.swift
+++ AquaBubbleBackground.swift
@@ -2,101 +2,103 @@
 
 import SwiftUI
 
-
-struct AquaBubbleBackground: View {
-    
-    enum AquaStyle {
-        case blueberry
-        case lime
-        case graphite
-    }
-    
-    init(style: AquaStyle = .blueberry, reversedGradient: Bool = false) {
-        
-        var colors: [Color]
-        switch style {
-        case .blueberry:
-            colors = [Color(red: 0.15, green: 0.5, blue: 0.8),
-                      Color(red: 0.5, green: 0.85, blue: 1.0)]
-        case .lime:
-            colors = [Color(red: 0.27, green: 0.6, blue: 0.12),
-                      Color(red: 0.4, green: 0.9, blue: 0.18)]
-        default:
-            colors = [Color(red: 0.4, green: 0.4, blue: 0.4),
-                      Color(red: 0.6, green: 0.6, blue: 0.6)]
-        }
-        
-        let gradient = Gradient(colors: reversedGradient ? colors.reversed() : colors)
-        self.baseGradient = LinearGradient(gradient: gradient, startPoint: .top, endPoint: .bottom)
-        
-    }
-    
-    private var baseGradient: LinearGradient
-    
-    private var glossGradient: LinearGradient = {
-        let colors: [Color] = [Color.white.opacity(0.6),
-                               Color.white.opacity(0.3),
-                               Color.white.opacity(0.0)]
-        let gradient = Gradient(colors: colors)
-        return LinearGradient(gradient: gradient, startPoint: .top, endPoint: .bottom)
-    }()
-    
-    
-    var body: some View {
-        return ZStack {
-            
-            self.baseGradient
-            
-            self.glossGradient
-                .mask(Capsule())
-                .padding(.top, 3.0)
-                .padding(.horizontal, 7.0)
-                .padding(.bottom, 9.0)
-                .blur(radius: 0.5)
-            
-        }
-        .overlay(
-            ZStack {
-                
-                Capsule() // Inner Shadow 1
-                    .strokeBorder(lineWidth: 2.0)
-                    .foregroundColor(Color.white)
-                    .blur(radius: 3.0)
-                    .opacity(1.0)
-                    .blendMode(.softLight)
-
-                Capsule() // Inner Shadow 2
-                    .strokeBorder(lineWidth: 1.0)
-                    .foregroundColor(Color.white)
-                    .blur(radius: 1.5)
-                    .opacity(0.8)
-                    .blendMode(.softLight)
-                
-                Capsule() // Border
-                    .strokeBorder(lineWidth: 1.0)
-                    .opacity(0.32)
-                
-            }
-        )
-        .mask(Capsule())
-        .shadow(color: Color.black.opacity(0.64), radius: 2.0, x: 0.0, y: 1.0)
-    }
-    
+enum AquaStyle {
+  case blueberry
+  case lime
+  case graphite
 }
 
-
+struct AquaBubbleBackground<S: InsettableShape>: View { // a Shape is a View
+  
+  let shape : S
+  
+  init(style: AquaStyle = .blueberry, reversedGradient: Bool = false,
+       shape: S)
+  {
+    self.shape = shape
+    
+    var colors: [Color]
+    switch style {
+      case .blueberry:
+        colors = [Color(red: 0.15, green: 0.5, blue: 0.8),
+                  Color(red: 0.5, green: 0.85, blue: 1.0)]
+      case .lime:
+        colors = [Color(red: 0.27, green: 0.6, blue: 0.12),
+                  Color(red: 0.4, green: 0.9, blue: 0.18)]
+      default:
+        colors = [Color(red: 0.4, green: 0.4, blue: 0.4),
+                  Color(red: 0.6, green: 0.6, blue: 0.6)]
+    }
+    
+    let gradient = Gradient(colors: reversedGradient ? colors.reversed() : colors)
+    self.baseGradient = LinearGradient(gradient: gradient,
+                                       startPoint: .top, endPoint: .bottom)
+    
+  }
+  
+  private var baseGradient: LinearGradient
+  
+  private var glossGradient: LinearGradient = {
+    let colors: [Color] = [Color.white.opacity(0.6),
+                           Color.white.opacity(0.3),
+                           Color.white.opacity(0.0)]
+    let gradient = Gradient(colors: colors)
+    return LinearGradient(gradient: gradient, startPoint: .top, endPoint: .bottom)
+  }()
+  
+  
+  var body: some View {
+    return ZStack {
+      
+      self.baseGradient
+      
+      self.glossGradient
+        .mask(shape) // yes, this is required
+        .padding(.top, 3.0)
+        .padding(.horizontal, 7.0)
+        .padding(.bottom, 9.0)
+        .blur(radius: 0.5)
+      
+    }
+    .overlay(
+      ZStack {
+        shape // Inner Shadow 1
+          .strokeBorder(lineWidth: 2.0)
+          .foregroundColor(Color.white)
+          .blur(radius: 3.0)
+          .opacity(1.0)
+          .blendMode(.softLight)
+        
+        shape // Inner Shadow 2
+          .strokeBorder(lineWidth: 1.0)
+          .foregroundColor(Color.white)
+          .blur(radius: 1.5)
+          .opacity(0.8)
+          .blendMode(.softLight)
+        
+        shape // Border
+          .strokeBorder(lineWidth: 1.0)
+          .opacity(0.32)
+        
+      }
+    )
+    .mask(shape)
+    .shadow(color: Color.black.opacity(0.64), radius: 2.0, x: 0.0, y: 1.0)
+  }
+}
 
 struct Background_Previews: PreviewProvider {
     static var previews: some View {
         VStack(spacing: 14.0) {
             
-            Text("What are you talking about?")
+            Text("What are you talking about?\nblub on more\nlines")
                 .padding([.vertical], 10.0)
                 .padding([.horizontal], 18.0)
                 .font(Font.body.weight(.medium))
                 .foregroundColor(Color.black)
                 .shadow(color: Color.white.opacity(0.8), radius: 1.5, x: 0.0, y: 1.0)
-                .background(AquaBubbleBackground(style: .blueberry))
+                .background(AquaBubbleBackground(style: .blueberry, 
+                              shape: RoundedRectangle(cornerSize: CGSize(width: 16, height: 16))))
             
             Text("What are you talking about?")
                 .padding([.vertical], 10.0)
@@ -104,7 +106,7 @@ struct Background_Previews: PreviewProvider {
                 .font(Font.body.weight(.medium))
                 .foregroundColor(Color.black)
                 .shadow(color: Color.white.opacity(0.7), radius: 1.5, x: 0.0, y: 1.0)
-                .background(AquaBubbleBackground(style: .lime))
+                .background(AquaBubbleBackground(style: .lime, shape: Capsule()))
                 .offset(x: 24.0)
             
             Text("What are you talking about?")
@@ -113,7 +115,7 @@ struct Background_Previews: PreviewProvider {
                 .font(Font.body.weight(.medium))
                 .foregroundColor(Color.black)
                 .shadow(color: Color.white.opacity(0.6), radius: 1.5, x: 0.0, y: 1.0)
-                .background(AquaBubbleBackground(style: .graphite, reversedGradient: true))
+                .background(AquaBubbleBackground(style: .graphite, reversedGradient: true, shape: Capsule()))
             
         }
         .padding(42.0)
